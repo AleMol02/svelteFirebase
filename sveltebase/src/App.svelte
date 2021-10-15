9d89d1154154a7af42831d2ec00ccca91cca7ff2
Enric Climent <enric612@gmail.com>
2021-10-15 09:38:07 +0200

    formulario plano sin código

--- sveltebase/src/App.svelte
+++ sveltebase/src/App.svelte
@@ -1,11 +1,28 @@
 <script>
-	
 </script>
 
 <main>
-	
+	<form>
+		<input
+			id="nombre-producto"
+			type="text"
+			placeholder="Nombre del producto"
+		/>
+		<textarea
+			id="descripcion-producto"
+			rows="3"
+			placeholder="Descripción del producto"
+		/>
+		<input
+			type="url"
+			id="imagen-url-producto"
+			placeholder="https://amazon.com/..."
+		/>
+		<button>
+			Guardar
+		</button>
+	</form>
 </main>
 
 <style>
-
 </style>
